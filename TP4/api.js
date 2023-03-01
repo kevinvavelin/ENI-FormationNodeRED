8eb99b228cb95b7473dd8b0c525f33ac41b63015
Kevin Vavelin <vavelinkevin@gmail.com>
2023-03-01 21:36:48 +0100

    Add file flows to our api json

--- TP4/api.js
+++ TP4/api.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 app.use('/', function(request, response) {
-    response.json();
+    var flowsFile = JSON.parse(fs.readFile("flows.json"));
+    response.json(flowsFile);
 });
 
-app.use("/toto", function(request, response) {
-
-});
 
 app.listen(4324, function() {
     console.log("Api is running");
